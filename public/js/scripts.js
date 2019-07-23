9ad5cedea70dd4386519bf56e71d195affd45a82
Pedro <schreiber.mmb@gmail.com>
2019-07-23 14:04:16 -0300

    updated active links on navbar

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -4,3 +4,8 @@ function initMap() {
     document.getElementById('map'), {zoom: 4, center: ju});
     var marker = new google.maps.Marker({position: ju, map: map});
 }
+
+$(document).ready(function() {
+    $('li.active').removeClass('active');
+    $('a[href="' + location.pathname + '"]').closest('li').addClass('active'); 
+  });
